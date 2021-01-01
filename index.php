c22740f9e9e692da96dda34f3be90b32f44c609b
Jaimy <jaimy@pc.me>
2021-01-01 22:49:44 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    <form method="post">
+        Password Length (max. 2048):
+        <input type="text" name="" id="">
+        Include lowercase characters:
+        <input type="checkbox" name="" id="">
+        Include uppercase characters:
+        <input type="checkbox" name="" id="">
+        Include numbers:
+        <input type="checkbox" name="" id="">
+        Include symbols:
+        <input type="checkbox" name="" id="">
+
+        <input type="submit" value="Generate my password">
+
+        Your new password:
+        <input type="text" name="" id="">
+        <input type="button" value="Copy">
+
+        Previous passwords:
+        Download json
+    </form>
+</body>
+</html>
